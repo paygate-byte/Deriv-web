932e09ee3b37a608499d1bafcb2ad40f36d0f553
pc <koskeicollins386@gmail.com>
2025-05-03 19:50:39 +0300

    Add files via upload

---
+++ root.ts
@@ -0,0 +1,29 @@
+import { postRouter } from "~/server/api/routers/post";
+import { botRouter } from "~/server/api/routers/bots";
+import { pdfRouter } from "~/server/api/routers/pdfs";
+import { adminRouter } from "~/server/api/routers/admin";
+import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
+
+/**
+ * This is the primary router for your server.
+ *
+ * All routers added in /api/routers should be manually added here.
+ */
+export const appRouter = createTRPCRouter({
+  post: postRouter,
+  bot: botRouter,
+  pdf: pdfRouter,
+  admin: adminRouter,
+});
+
+// export type definition of API
+export type AppRouter = typeof appRouter;
+
+/**
+ * Create a server-side caller for the tRPC API.
+ * @example
+ * const trpc = createCaller(createContext);
+ * const res = await trpc.post.all();
+ *       ^? Post[]
+ */
+export const createCaller = createCallerFactory(appRouter);
